6e725994b00160342bc30f1e4f220d0cebc9ce6e
KaioRapanos <kaiorapanos@gmail.com>
2023-08-18 21:22:14 -0300

    Aula 4

---
+++ src/list/Ordenacao/Pessoa.java
@@ -0,0 +1,43 @@
+package list.Ordenacao;
+
+import java.util.Comparator;
+
+public class Pessoa implements Comparable<Pessoa> {
+    //atributos
+    private String nome;
+    private Integer idade;
+    private Double altura;
+
+    public Pessoa(String nome, Integer idade1, Double altura) {
+        this.nome = nome;
+        this.idade = idade1;
+        this.altura = altura;
+    }
+
+
+    @Override
+    public int compareTo(Pessoa p) {
+        return Integer.compare(idade, p.getIdade());
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public Integer getIdade() {
+        return idade;
+    }
+
+    public Double getAltura() {
+        return altura;
+    }
+
+    @Override
+    public String toString() {
+        return "Pessoa{" +
+                "idade='" + idade + '\'' +
+                ", idade=" + idade +
+                ", alturta=" + altura +
+                '}';
+    }
+}
